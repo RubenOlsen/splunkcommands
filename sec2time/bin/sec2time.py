cfe8c732a5eb08a635601942a34d9daaeea292b9
Ruben Olsen <ruben@azuralis.no>
2011-12-01 16:06:42 +0100

    sec2time is now now also handling days

--- sec2time/bin/sec2time.py
+++ sec2time/bin/sec2time.py
@@ -1,13 +1,27 @@
-# coding=utf-8
-
+# Usage: 
 #
-# echo
+# sec2time infield=<input field name> outfield=<output field name>
 #
-# Returns command
 #
-# Usage:
-#
-#   echo field="name" value="value"
+# Copyright (C) 2011 Ove Ruben R Olsen Laerk
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 import sys
 import splunk.Intersplunk
@@ -15,7 +29,10 @@ import splunk.Intersplunk
 from time import strftime, gmtime, time
 
 def sec2time(seconds):
-    time_string = strftime("%H:%M:%S", gmtime(seconds))
+    if (seconds > 86400):
+      time_string = strftime("%j %H:%M:%S", gmtime(seconds))
+    else:
+      time_string = strftime("%H:%M:%S", gmtime(seconds))
     return time_string
 
 
